cb93d1489322d5f4ae907a8e04a4e5c6a6df51f8
Waterstrong <sqlin@thoughtworks.com>
2017-02-23 23:29:41 +0800

    should cell next status be dead given one alive neighbors

--- src/test/java/tw/conway/domain/CellTest.java
+++ src/test/java/tw/conway/domain/CellTest.java
@@ -5,18 +5,36 @@ import static org.junit.Assert.assertThat;
 import static tw.conway.enumeration.LifeStatus.DEAD;
 import static tw.conway.enumeration.LifeStatus.LIVE;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CellTest {
+
+    private Cell liveCell;
+    private Cell deadCell;
+
+    @Before
+    public void setUp() {
+        liveCell = new Cell(LIVE);
+        deadCell = new Cell(DEAD);
+    }
+
     @Test
     public void shouldNextStatusBeDeadGivenZeroAliveNeighbors() {
-        Cell aliveCell = new Cell(LIVE);
-        Cell deadCell = new Cell(DEAD);
-
-        aliveCell.nextGeneration(0);
+        liveCell.nextGeneration(0);
         deadCell.nextGeneration(0);
 
-        assertThat(aliveCell.getStatus(), is(DEAD));
+        assertThat(liveCell.getStatus(), is(DEAD));
         assertThat(deadCell.getStatus(), is(DEAD));
     }
+
+    @Test
+    public void shouldNextStatusBeDeadGivenOneAliveNeighbors() {
+        liveCell.nextGeneration(1);
+        deadCell.nextGeneration(1);
+
+        assertThat(liveCell.getStatus(), is(DEAD));
+        assertThat(deadCell.getStatus(), is(DEAD));
+    }
+
 }
