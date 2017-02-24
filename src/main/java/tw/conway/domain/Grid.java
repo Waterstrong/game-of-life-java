edb5e2bc051fe4ce265dc0fa70ee87094266de5a
Waterstrong <sqlin@thoughtworks.com>
2017-02-25 00:21:32 +0800

    refactor the cell map to array and add next generation function

--- src/main/java/tw/conway/domain/Grid.java
+++ src/main/java/tw/conway/domain/Grid.java
@@ -7,37 +7,51 @@ import static tw.conway.enumeration.LifeStatus.DEAD;
 import static tw.conway.enumeration.LifeStatus.LIVE;
 
 import java.awt.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Grid {
     private final int width;
     private final int height;
-    private final Map<Integer, Map<Integer, Cell>> cells = new HashMap<>();
+    private final Cell[][] cells;
     private static final int[][] NEIGHBOR_INDEX = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
 
     public Grid(int width, int height, Point... aliveCellsPosition) {
         this.width = width;
         this.height = height;
+        cells = new Cell[width][height];
         initCellStatus(aliveCellsPosition);
     }
 
     private void initCellStatus(Point... aliveCellsPosition) {
-        range(0, width).forEach(w -> {
-            Map<Integer, Cell> cellMap = new HashMap<>();
-            range(0, height).forEach(h -> cellMap.put(h, new Cell(DEAD)));
-            cells.put(w, cellMap);
-        });
-        stream(ofNullable(aliveCellsPosition).orElse(new Point[]{})).forEach(point -> cells.get(point.x).get(point.y).setStatus(LIVE));
+        range(0, width).forEach(w -> range(0, height).forEach(h -> cells[w][h] = new Cell(DEAD)));
+        stream(ofNullable(aliveCellsPosition).orElse(new Point[]{}))
+                .filter(point -> isWithinRange(point))
+                .forEach(point -> cells[point.x][point.y].setStatus(LIVE));
+    }
+
+    private boolean isWithinRange(Point point) {
+        return point.x >= 0 && point.x < width && point.y >= 0 && point.y < height;
     }
 
     public int getAliveNeighborNumber(Point point) {
-        return (int) stream(NEIGHBOR_INDEX)
-                .filter(index -> LIVE.equals(cells.get(point.x + index[0]).get(point.y + index[1]).getStatus()))
-                .count();
+        return (int) stream(NEIGHBOR_INDEX).filter(index -> {
+            Point newPoint = new Point(point.x + index[0], point.y + index[1]);
+            return isWithinRange(newPoint) && LIVE.equals(cells[(newPoint.x)][newPoint.y].getStatus());
+        }).count();
     }
 
     public Cell getCellAt(Point point) {
-        return cells.getOrDefault(point.x, new HashMap<>()).getOrDefault(point.y, null);
+        return isWithinRange(point) ? cells[point.x][point.y] : null;
+    }
+
+    public void nextGeneration() {
+        range(0, width).forEach(w -> range(0, height).forEach(h -> cells[w][h].nextGeneration(getAliveNeighborNumber(new Point(w, h)))));
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
--- src/test/java/tw/conway/domain/GridTest.java
+++ src/test/java/tw/conway/domain/GridTest.java
@@ -13,10 +13,12 @@ import org.junit.Test;
 public class GridTest {
     @Test
     public void shouldInitTheAliveCellsInTheGrid() {
-        Point[] aliveCellsPosition = {new Point(0, 0), new Point(1, 1)};
+        Point[] aliveCellsPosition = {new Point(0, 0), new Point(1, 1), new Point(3, 3)};
 
         Grid grid = new Grid(3, 3, aliveCellsPosition);
 
+        assertThat(grid.getWidth(), is(3));
+        assertThat(grid.getHeight(), is(3));
         assertThat(grid.getCellAt(new Point(0, 0)).getStatus(), is(LIVE));
         assertThat(grid.getCellAt(new Point(1, 1)).getStatus(), is(LIVE));
         assertThat(grid.getCellAt(new Point(2, 1)).getStatus(), is(DEAD));
@@ -33,7 +35,6 @@ public class GridTest {
         assertNull(grid.getCellAt(new Point(-1, 1)));
         assertNull(grid.getCellAt(new Point(1, -1)));
         assertNull(grid.getCellAt(new Point(-1, -1)));
-
     }
 
     @Test
@@ -47,5 +48,27 @@ public class GridTest {
         int aliveNeighborNumber = grid.getAliveNeighborNumber(new Point(1, 1));
 
         assertThat(aliveNeighborNumber, is(8));
+    }
+
+    @Test
+    public void shouldNotGetTheAliveNeighborNumberWhenTargetPointIsOutOfRange() {
+        Point[] aliveCellsPosition = {new Point(0, 0)};
+        Grid grid = new Grid(3, 3, aliveCellsPosition);
+
+        int aliveNeighborNumber = grid.getAliveNeighborNumber(new Point(3, 3));
+
+        assertThat(aliveNeighborNumber, is(0));
+    }
+
+    @Test
+    public void shouldExecuteNextGenerationCorrectly() {
+        Point[] aliveCellsPosition = {new Point(0, 1), new Point(1, 0),
+                new Point(1, 1), new Point(1, 2), new Point(2, 1),};
+        Grid grid = new Grid(3, 3, aliveCellsPosition);
+
+        grid.nextGeneration();
+
+        assertThat(grid.getCellAt(new Point(0, 0)).getStatus(), is(LIVE));
+        assertThat(grid.getCellAt(new Point(1, 1)).getStatus(), is(DEAD));
     }
 }
